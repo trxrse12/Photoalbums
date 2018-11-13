2326f9f23260832dd178426e6e9bbd51eb69da7d
R Sepp <trxrse@gmail.com>
2018-11-13 11:48:26 +0000

    13/Nov/2018 H1. Added on OpsWorks database

---
+++ config/local.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: {
+        host					: 'localhost',
+        database 				: 'photoalbums_database',
+        user     				: 'root',
+        password 				: 'test1234'
+    }
+};
